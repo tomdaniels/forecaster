bdb942ff038663af86541ccaf50b40c1a3a0355c
tomdaniels <thomsondaniels@hotmail.com>
2021-12-07 17:10:29 +1100

    location api

--- src/api/__tests__/location-api.test.js
+++ src/api/__tests__/location-api.test.js
@@ -1,7 +1,20 @@
 import locationApi from '..';
+import * as fetch from '../fetch';
 
 describe('location-api.js', () => {
-  it('', () => {
+  it('exposes network requests as functions', () => {
     expect(locationApi).toHaveProperty('get');
+  });
+
+  it('passes on the network response', async () => {
+    const fetchSpy = jest.spyOn(fetch, 'default');
+    const dummyResponse = {
+      text: 'it worked',
+    };
+    fetchSpy.mockResolvedValue(dummyResponse);
+    const response = await locationApi.get('https://www.xbox');
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(response).toEqual(dummyResponse);
   });
 });
